262745c3204950a8638bec811c517ce727530475
nikolay102 <k.sawchenckov@yandex.ru>
2026-01-09 17:11:17 +0500

    Mega revert

--- main/main/views.py
+++
@@ -1,8 +0,0 @@
-from django.shortcuts import render
-from django.http import HttpResponse
-
-def index(req):
-    return render(req,'main/index.html')
-
-def about(req):
-    return HttpResponse('<h4>About</h4>')
